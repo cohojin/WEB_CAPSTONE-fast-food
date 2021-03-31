a283170e0fe3ae628db0fd54666058ca47677d0a
김민호 <49149127+rlaalsgh@users.noreply.github.com>
2021-03-31 23:21:53 +0900

    프론트 단 javascaript

--- login-lecture/app/app.js
+++ login-lecture/app/app.js
@@ -10,6 +10,7 @@ const PORT = 3000;
 // 앱세팅
 app.set("views","./src/views");
 app.set("view engine","ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 
 app.use("/",home); // use -> 미들웨어를 등록하는 메소드
