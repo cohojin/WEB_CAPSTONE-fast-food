993ca6bb65a3ea7a2afb1a1ac9ebb9985002bf22
김민호 <49149127+rlaalsgh@users.noreply.github.com>
2021-04-07 21:40:10 +0900

    폴더구조 변경

--- login-lecture/app/src/routes/home/home.ctrl.js
+++ login-lecture/app/src/routes/home/home.ctrl.js
@@ -14,24 +14,24 @@ const output = {
 const process = {
     login:(req,res)=>{
 
-    //    const user = new User(req.body);
-    //    const respose = user.login();
-    //     return res.json(respose);
+       const user = new User(req.body);
+       const respose = user.login();
+        return res.json(respose);
 
-        const id = req.body.id,
-            psword = req.body.psword;
+    //     const id = req.body.id,
+    //         psword = req.body.psword;
 
-        const userStorage = new Userstrorage();
-        console.log(userStorage.users);
+    //     const userStorage = new Userstrorage();
+    //     console.log(userStorage.users);
     //    const users =  Userstrorage.getUsers("id","psword");
     //     const respose = {};
-        // if(users.id.includes(id)){
-        //     const idx = users.id.indexOf(id);
-        //     if(users.psword[idx] === psword) {
-        //         respose.success = true;
-        //         return res.json(respose);
-        //     }
-        // }
+    //     if(users.id.includes(id)){
+    //         const idx = users.id.indexOf(id);
+    //         if(users.psword[idx] === psword) {
+    //             respose.success = true;
+    //             return res.json(respose);
+    //         }
+    //     }
         respose.success = false;
         respose.msg = "로그인에 실패했습니다."
         return res.json(respose);
