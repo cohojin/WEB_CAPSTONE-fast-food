a33a6a3c0428b2fe1820e36d9cfde55a5ee30b6d
cohojin <kimhojin3714@naver.com>
2021-04-28 23:17:24 +0900

    ProductPage 작업

--- Pages/re_main/menubar_fixed.js
+++ Pages/re_main/menubar_fixed.js
@@ -1,10 +1,11 @@
-var category_wrap = $("#category_wrap").offset().top;
-
-$(window).scroll(function() {
-    var window = $(this).scrollTop();
-    if(category_wrap<=window){
-        $("#category_wrap").addClass("fixed");
-    }else{
-        $("#category_wrap").removeClass("fixed");
-    }
-})
+$(function(){
+    var lnb = $("#category_wrap").offset().top;
+    $(window).scroll(function() {
+        var window = $(this).scrollTop();
+        if(lnb<=window){
+            $("#category_wrap").addClass("fixed");
+        }else{
+            $("#category_wrap").removeClass("fixed");
+        }
+    })
+});
