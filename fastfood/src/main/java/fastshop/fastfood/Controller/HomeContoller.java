a4e75dc533f04c73beccc12479f9f14126cf00ae
김민호 <49149127+rlaalsgh@users.noreply.github.com>
2021-05-10 21:08:54 +0900

    폴더구조 변경및 Back 추가작업

---
+++ fastfood/src/main/java/fastshop/fastfood/Controller/HomeContoller.java
@@ -0,0 +1,12 @@
+package fastshop.fastfood.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeContoller {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
--- fastfood/src/main/java/fastshop/fastfood/Repository/MemberRepository.java
+++ fastfood/src/main/java/fastshop/fastfood/Repository/MemberRepository.java
@@ -7,23 +7,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.*;
 
-@Repository
-public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    @Transactional
-    public Long save(Member member) {
-
-        em.persist(member);
-
-        return member.getId();
-    }
-
-    public Member find(Long id) {
-
-        return em.find(Member.class, id);
-    }
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
 }
